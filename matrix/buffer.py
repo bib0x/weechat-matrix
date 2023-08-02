894afd349f0d7d167ff3acf8a3e470b1a0b038f4
bib0x <bib0x.github@bootsect.org>
2023-08-02 11:47:44 +0200

    force displayname usage for buffer room names

--- matrix/buffer.py
+++ matrix/buffer.py
@@ -1076,7 +1076,9 @@ class RoomBuffer(object):
                 user.user_id.startswith("@signal_") or
                 user.user_id.startswith("@_telegram_") or
                 user.user_id.startswith("@_xmpp_") or
-                user.user_id.startswith("@irc_")):
+                user.user_id.startswith("@irc_") or 
+                (user.user_id.startswith("@") and user.user_id.endswith(self.homeserver))
+            ):
             if user.display_name:
                 short_name = user.display_name[0:50]
         elif user.user_id.startswith("@twilio_"):
